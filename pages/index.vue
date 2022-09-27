821bfad2e4217c5db22e3d39eba9c6297f0ad6e3
Yusa <yusa.oruc@stu.fsm.edu.tr>
2022-09-27 19:41:27 +0300

    api kullanımı

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- CSS only -->
+  <Users />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Users from '~/components/Users.vue';
+
+export default Vue.extend({
+    name: "IndexPage",
+    components: { Users }
+})
+</script>
